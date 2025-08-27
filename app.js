bb69b548a592f415de90306b8b649ba9f5d2fdf4
Yamil <yamilosmarr@gmail.com>
2025-08-26 20:22:11 -0400

    prueba-de-sincronizacion

---
+++ app.js
@@ -0,0 +1,21 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Ingresar nombre de varios amigos y luego realizar un sorteo aleatorio para determinar quién es el amigo secreto.
+//La aplicación debe permitir que se agreguen nombres a través de un campo de texto y apretando el botón añadir
+//Si un usuario intena ingresar un nombre vacío deberá aparecer una alerta indicando que debe ingresarse un nombre válido
+//Al apretar el botón añadir amigo, la app deberá escoger un nombre aleatorio y mostrarlo en pantalla.
+        //Agregar nombres, Validar entrada, Visualizar lista, Sorteo aleatorio.
+function condicionesIniciales(){
+    vaciarcaja()
+}
+function vaciarcaja(){
+
+}
+
+function agregarAmigo(){
+
+}
+
+function sortearAmigo(){
+
+}
+const listaAmigos = [];
